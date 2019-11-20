2e8b52e23032e4db127c476dbc9d164f56dabecb
Lodi Dodevska <lodi.dodevska@sorsix.com>
2019-11-20 23:01:28 +0100

    exception handling

--- api/src/main/java/com/fri/code/outputs/v1/resources/OutputMetadataResource.java
+++ api/src/main/java/com/fri/code/outputs/v1/resources/OutputMetadataResource.java
@@ -5,6 +5,7 @@ import com.fri.code.outputs.lib.CompilerOutput;
 import com.fri.code.outputs.lib.InputMetadata;
 import com.fri.code.outputs.lib.OutputMetadata;
 import com.fri.code.outputs.services.beans.OutputMetadataBean;
+import com.fri.code.outputs.v1.dtos.ApiError;
 import javassist.tools.reflect.Compiler;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -40,7 +41,11 @@ public class OutputMetadataResource {
             return Response.ok(output).build();
         }
         catch (Exception e){
-            return Response.status(Response.Status.NOT_FOUND).build();
+            ApiError apiError = new ApiError();
+            apiError.setCode(Response.Status.NOT_FOUND.toString());
+            apiError.setMessage(e.getMessage());
+            apiError.setStatus(Response.Status.NOT_FOUND.getStatusCode());
+            return Response.status(Response.Status.NOT_FOUND).entity(apiError).build();
         }
     }
 
@@ -53,27 +58,52 @@ public class OutputMetadataResource {
             return Response.ok(outputs).build();
         }
         catch (Exception e){
-            return Response.status(Response.Status.NOT_FOUND).build();
+            ApiError apiError = new ApiError();
+            apiError.setCode(Response.Status.NOT_FOUND.toString());
+            apiError.setMessage(e.getMessage());
+            apiError.setStatus(Response.Status.NOT_FOUND.getStatusCode());
+            return Response.status(Response.Status.NOT_FOUND).entity(apiError).build();
         }
 
     }
 
     @POST
     public Response createOutput(OutputMetadata outputMetadata){
+
         if (outputMetadata.getInputID() == null || outputMetadata.getCorrectOutput() == null || outputMetadata.getUserOutput() == null){
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
+
         else{
-            outputMetadata = outputMetadataBean.createOutputMetadata(outputMetadata);
-            return Response.status(STATUS_OK).entity(outputMetadata).build();
+            try {
+                outputMetadata = outputMetadataBean.createOutputMetadata(outputMetadata);
+                return Response.status(STATUS_OK).entity(outputMetadata).build();
+            }
+            catch (Exception e){
+                ApiError apiError = new ApiError();
+                apiError.setCode(Response.Status.BAD_REQUEST.toString());
+                apiError.setMessage(e.getMessage());
+                apiError.setStatus(Response.Status.BAD_REQUEST.getStatusCode());
+                return Response.status(Response.Status.BAD_REQUEST).entity(apiError).build();
+            }
         }
+
     }
 
     @POST
     @Path("results")
     public Response getResults(List<InputMetadata> inputs){
-        Map<Integer, Boolean> outputs = outputMetadataBean.getCompilerOutputsForExercise(inputs);
-        return Response.status(Response.Status.OK).entity(outputs).build();
+        try{
+            Map<Integer, Boolean> outputs = outputMetadataBean.getCompilerOutputsForExercise(inputs);
+            return Response.status(Response.Status.OK).entity(outputs).build();
+        }
+        catch (Exception e){
+            ApiError apiError = new ApiError();
+            apiError.setCode(Response.Status.INTERNAL_SERVER_ERROR.toString());
+            apiError.setMessage(e.getMessage());
+            apiError.setStatus(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(apiError).build();
+        }
     }
 
 
