950f5c1f4a296347ea8ed08454f7dc52f4edc52a
Lodi Dodevska <lodi.dodevska@sorsix.com>
2019-12-01 14:08:46 +0100

    added deployment.yaml

--- api/src/main/java/com/fri/code/outputs/v1/CodeOutputsApplication.java
+++ api/src/main/java/com/fri/code/outputs/v1/CodeOutputsApplication.java
@@ -1,8 +1,11 @@
 package com.fri.code.outputs.v1;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
+@RegisterService
 public class CodeOutputsApplication extends Application {
 }
