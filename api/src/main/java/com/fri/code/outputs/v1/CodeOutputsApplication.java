eb7889cfce76d0891f984ff091d95e39ee308e1d
Lodi Dodevska <lodi.dodevska@sorsix.com>
2020-01-16 03:36:25 +0100

    changed config

--- api/src/main/java/com/fri/code/outputs/v1/CodeOutputsApplication.java
+++ api/src/main/java/com/fri/code/outputs/v1/CodeOutputsApplication.java
@@ -11,7 +11,8 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
-@OpenAPIDefinition(info = @Info(title = "OutputsAPI", version = "v1.0.0", contact = @Contact()), servers = @Server(url = "http://34.70.28.108:8080/v1"))
+@OpenAPIDefinition(info = @Info(title = "OutputsAPI", version = "v1.0.0", contact = @Contact()),
+        servers = @Server(url = "http://34.70.28.108:8080/v1"))
 @RegisterService
 public class CodeOutputsApplication extends Application {
 }
